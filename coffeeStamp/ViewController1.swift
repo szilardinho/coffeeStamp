07b8906b25aaed7723495451405acf0ce02f6004
szilardinho <szilardinhog@gmail.com>
2017-05-03 22:06:44 -0400

    updated wkweb

--- coffeeStamp/ViewController1.swift
+++ coffeeStamp/ViewController1.swift
@@ -15,6 +15,7 @@ class ViewController1: UIViewController {
 
         // Do any additional setup after loading the view.
         
+    
        
     }
 
--- coffeeStamp/ViewController2.swift
+++ coffeeStamp/ViewController2.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController2: UIViewController {
+class ViewController2: UIViewController,WKUIDelegate,WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let myBlog = "http://www.oddfoxcoffee.com"
+        let url = URL(string: myBlog)
+        let request = URLRequest(url: url!)
+        
+        // init and load request in webview.
+        webView = WKWebView(frame: self.view.frame)
+        webView.navigationDelegate = self
+        webView.load(request)
+        self.view.addSubview(webView)
+        self.view.sendSubview(toBack: webView)
+        
+      
 
-        // Do any additional setup after loading the view.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
 
 
